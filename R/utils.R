8b230b3dfd44f84da4335d058bfe6f52380deb3b
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-08-24 16:09:46 -0400

    restart repo

--- R/utils.R
+++
@@ -1,8 +0,0 @@
-
-## Create or get the BiocFileCache object for the package
-.get_cache <- function() {
-    cache <- tools::R_user_dir('bugphyzzAnalyses', which = 'cache')
-    BiocFileCache::BiocFileCache(cache)
-}
-
-#' Convert con
