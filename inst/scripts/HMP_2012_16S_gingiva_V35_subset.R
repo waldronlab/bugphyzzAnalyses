b29c488f82b153236c26631e1b242be0b7cdef01
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-10-31 10:33:40 -0400

    update script for generating taxid table for v35 subset

--- inst/scripts/HMP_2012_16S_gingiva_V35_subset.R
+++ inst/scripts/HMP_2012_16S_gingiva_V35_subset.R
@@ -1,12 +1,12 @@
 
 ## This script is for getting the taxids at the genus level
 ## of the OTU's of the HMP_2012_16S_gingival_V35_subset dataset
-## In MicrobiomeBenchmarkData
+## In MicrobiomeBenchmarkData in row_data format.
 
-library(MicrobiomeBenchmarkData) # github: waldronlab/MicrobiomeBenchmarkData
+library(MicrobiomeBenchmarkData)
 library(taxize)
 library(dplyr)
-# library(bugphyzzAnalyses) #
+library(bugphyzzAnalyses)
 
 dat_name <- 'HMP_2012_16S_gingival_V35_subset'
 
@@ -17,21 +17,18 @@ row_data <- tse |>
     tibble::rownames_to_column('OTU') |>
     tibble::as_tibble()
 
-new_row_data <- taxNames2TaxIDs(df = row_data, names_from = 'OTU')
+new_row_data <- taxTable2taxid(df = row_data, names_from = 'OTU')
+
 if (all(rownames(tse) == new_row_data$otu)) {
     message('all good')
     rownames(new_row_data) <- new_row_data$otu
+    new_row_data <- select(new_row_data, -otu)
+    new_row_data <- relocate(new_row_data, taxon_annotation, .after = 'genus')
 }
-new_row_data <- select(new_row_data, -otu)
-new_row_data <- relocate(new_row_data, taxon_annotation, .after = 'genus')
 
 ## Export files
-
 output_file <- paste0(dat_name, '_taxids.tsv')
 write.table(
     x = new_row_data, file = paste0('inst/extdata/', output_file),
     row.names = TRUE, sep = '\t'
 )
-
-
-
