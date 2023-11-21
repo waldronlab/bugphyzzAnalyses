9b917be8c18df97184af0e9837b1d0492022fbfb
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-11-21 18:08:05 -0500

    update Rscript

--- R/get_taxonomy_for_bugphyzz.R
+++ R/get_taxonomy_for_bugphyzz.R
@@ -2,17 +2,12 @@ library(bugphyzz)
 library(taxizedb)
 library(purrr)
 library(dplyr)
-
 bp <- importBugphyzz(force_download = TRUE)
 taxids <- unique(as.character(bp$NCBI_ID))
-
 system.time({
    taxonomy <- classification(taxids, db = 'ncbi')
 })
-
-
 valRanks <- c('superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species', 'strain')
-
 system.time({
     x <- map(taxonomy, ~ {
         dat <- .x |>
@@ -28,11 +23,9 @@ system.time({
 
 })
 taxonomy_ids <- bind_rows(x, .id = 'NCBI_ID')
-
 write.table(
     x = taxonomy_ids, file = 'inst/extdata/taxonomy_ids.tsv', sep = '\t'
 )
-
 system.time({
     y <- map(taxonomy, ~ {
         dat <- .x |>
